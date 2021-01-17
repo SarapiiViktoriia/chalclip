c108376a26eac72e0344c021f98613f69e00ffe9
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-01-17 14:22:56 +0000

    experient with timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { Countdown } from './../helper/timer';
 import { InventoryHandler } from '../handlers/inventory-handler';
 import { MoveDirection } from './../modells/move-direction';
 import { Player } from '../modells/gameBlocks/player';
@@ -5,6 +6,7 @@ import { GameBlock } from '../modells/gameBlocks/game-block';
 import { LevelHandlerService } from '../handlers/level-handler.service';
 import { Component } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,15 +14,21 @@ import { HostListener } from '@angular/core';
 })
 export class HomePage {
   levelHandlerService: LevelHandlerService;
+  modalController: ModalController;
   items: Array<Array<Array<GameBlock>>>;
   inventory: InventoryHandler;
-  time: string = "not implemented"; 
-  energy: number = 0;
+  time: Countdown = new Countdown();
   battery: number = 0;
-  constructor(levelHandlerService: LevelHandlerService) {
+  energy: number = 0;
+  constructor(levelHandlerService: LevelHandlerService, modalController: ModalController) {
     this.levelHandlerService = levelHandlerService;
     this.items = this.levelHandlerService.getStack();
     this.inventory = this.levelHandlerService.getInventory();
+    this.modalController = modalController;
+    this.time.on("expired", () => this.presentModal());
+    this.time.start(10);
+  }
+  async presentModal() {
   }
   @HostListener('document:keydown', ['$event'])
   keyListenerEvent(event: KeyboardEvent) {
