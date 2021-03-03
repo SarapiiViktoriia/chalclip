2684ca78e6eab3282fce2ef92f17b9fede77e35f
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-03-03 02:52:07 +0000

    rename some variables and refactor them

--- src/app/handlers/level.service.ts
+++ src/app/handlers/level.service.ts
@@ -6,31 +6,28 @@ const STORAGE_KEY = 'levels';
 })
 export class LevelService {
   constructor(public storage: Storage) { }
-  isSaved(levelId) {
-    return this.getAllSavedLevels().then(result => {
-      return result && result.indexOf(levelId) !== -1;
-    });
+  public async isSaved(levelName: string) {
+    const result1 = await this.getAllSavedLevels();
+    return result1 && result1.indexOf(levelName) !== -1;
   }
-  saveLevel(levelId) {
-    return this.getAllSavedLevels().then(result => {
-      if (result) {
-        result.push(levelId);
-        return this.storage.set(STORAGE_KEY, result);
-      } else {
-        return this.storage.set(STORAGE_KEY, [levelId]);
-      }
-    });
+  public async saveLevel(levelName: string) {
+    const result1 = await this.getAllSavedLevels();
+    if (result1) {
+      result1.push(levelName);
+      return this.storage.set(STORAGE_KEY, result1);
+    } else {
+      return this.storage.set(STORAGE_KEY, [levelName]);
+    }
   }
-  deleteLevel(levelId) {
-    return this.getAllSavedLevels().then(result => {
-      if (result) {
-        const index = result.indexOf(levelId);
-        result.splice(index, 1);
-        return this.storage.set(STORAGE_KEY, result);
-      }
-    });
+  public async deleteLevel(levelName: string) {
+    const result1 = await this.getAllSavedLevels();
+    if (result1) {
+      const index = result1.indexOf(levelName);
+      result1.splice(index, 1);
+      return this.storage.set(STORAGE_KEY, result1);
+    }
   }
-  getAllSavedLevels() {
+  public getAllSavedLevels() {
     return this.storage.get(STORAGE_KEY);
   }
 }
