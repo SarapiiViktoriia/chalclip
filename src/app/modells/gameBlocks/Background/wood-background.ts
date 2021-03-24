5bfd66c0c57382033cbab40835642b0e04de368f
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-03-24 05:44:24 +0000

    merge contributing into doc

--- src/app/modells/gameBlocks/Background/wood-background.ts
+++ src/app/modells/gameBlocks/Background/wood-background.ts
@@ -1,20 +1,12 @@
 import { MoveDirection } from './../../move-direction';
 import { LevelHandlerService } from '../../../handlers/level-handler.service';
 import { GameBlock } from '../game-block';
-import { StackLayer } from '../../stackLayer';
 export class WoodBackground extends GameBlock {
   constructor(levelHandler: LevelHandlerService) {
     super(levelHandler);
-    this.$imageSource = 'assets/background/wood.bmp';
+    this.$imageSource = "assets/background/wood.bmp";
   }
-  public name = 'Wood Background';
   public canMoveToHere(blockToMove: GameBlock, direction: MoveDirection) {
     return true;
-  }
-  public getStackZCoord(): StackLayer {
-    return StackLayer.texture;
-  }
-  public getInstance(levelHandler: LevelHandlerService) {
-    return new WoodBackground(levelHandler);
   }
 }
--- src/app/modells/gameBlocks/moveable-block.ts
+++ src/app/modells/gameBlocks/moveable-block.ts
@@ -1,26 +1,19 @@
 import { MoveDirection } from './../move-direction';
 import { GameBlock } from './game-block';
 import { LevelHandlerService } from 'src/app/handlers/level-handler.service';
-import { StackLayer } from '../stackLayer';
 export class MoveableBlock extends GameBlock {
   constructor(levelHandler: LevelHandlerService) {
     super(levelHandler);
-    this.$imageSource = 'assets/P_stone.bmp';
+    this.$imageSource = "assets/P_stone.bmp";
   }
-  public name = 'Dirt';
   public canMoveToHere(blockToMove: GameBlock, direction: MoveDirection) {
-    const currentPosition = this.levelHandler.getBlockPosition(this);
-    const currentPositionStack = this.levelHandler.getZStack(currentPosition);
-    const lastPosition = this.levelHandler.getLastPosition(currentPosition, direction);
-    const lastPositionStack = this.levelHandler.getZStack(lastPosition);
-    if (!lastPositionStack.includes(this.levelHandler.player)) {
-      return false;
-    }
-    const newPosition = this.levelHandler.getNewPosition(currentPosition, direction);
+    let currentPosition = this.levelHandler.getBlockPosition(this);
+    let currentPositionStack = this.levelHandler.getZStack(currentPosition[0], currentPosition[1]);
+    let newPosition = this.levelHandler.getNewPosition(currentPosition, direction);
     if (newPosition.includes(-1) || currentPosition === newPosition) {
       return false;
     }
-    const newPositionStack = this.levelHandler.getZStack(newPosition);
+    let newPositionStack = this.levelHandler.getZStack(newPosition[1], newPosition[0]);
     if (!this.levelHandler.executeCanMoveFromHere(this, direction, currentPositionStack)) {
       return false;
     }
@@ -33,11 +26,5 @@ export class MoveableBlock extends GameBlock {
     if (blockToMove !== this) {
       this.levelHandler.moveBlock(this, direction);
     }
-  }
-  public getInstance(levelHandler: LevelHandlerService): MoveableBlock {
-    return new MoveableBlock(levelHandler);
-  }
-  public getStackZCoord(): StackLayer {
-    return StackLayer.block;
   }
 }
