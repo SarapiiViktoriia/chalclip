c4a5292b66861e1f5b940a97f8c73889456ca8ee
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-04-12 22:39:34 +0000

    rename typso inventorySize

--- src/app/handlers/inventory-handler.service.ts
+++ src/app/handlers/inventory-handler.service.ts
@@ -7,15 +7,15 @@ import { GameBlock } from '../models/gameBlocks/game-block';
 })
 export class InventoryHandlerService {
   private inventory: Array<GameBlock> = new Array<GameBlock>(0);
-  private inventrySize = 8;
+  private inventorySize = 8;
   constructor(levelHandler: LevelHandlerService) {
-    for (let inventoryItem = 0; inventoryItem < this.inventrySize; inventoryItem++) {
+    for (let inventoryItem = 0; inventoryItem < this.inventorySize; inventoryItem++) {
       this.inventory.push(new EmptyBlock(levelHandler));
     }
   }
   public getInventoryItems(elementsPerArray: number): Array<Array<GameBlock>> {
     const dimensionalInventory = new Array<Array<GameBlock>>(0);
-    for (let index = 0; index < (this.inventrySize / elementsPerArray); index++) {
+    for (let index = 0; index < (this.inventorySize / elementsPerArray); index++) {
       dimensionalInventory[index] = new Array<GameBlock>(0);
       const elements = this.inventory.slice(index * elementsPerArray, index * elementsPerArray + elementsPerArray);
       dimensionalInventory[index] = elements;
