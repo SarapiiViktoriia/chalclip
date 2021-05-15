5786c14ebce3a2e81b12eddb7334f46f80518189
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-05-15 12:47:40 +0000

    WIP Refactor level grid into extra component

--- src/app/leveleditor/leveleditor.module.ts
+++ src/app/leveleditor/leveleditor.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { LeveleditorPage } from './leveleditor.page';
+import { LevelGridComponent } from '../level/level-grid/level-grid.component';
 const routes: Routes = [
   {
     path: '',
@@ -17,6 +18,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [LeveleditorPage]
+  declarations: [
+    LeveleditorPage,
+    LevelGridComponent
+  ]
 })
 export class LeveleditorPageModule {}
